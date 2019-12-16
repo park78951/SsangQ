236c165dc62f1d84d331d9459e40737bfc734536
Sangkyu <park78951@gmail.com>
2019-12-16 13:34:13 +0900

    docs: add google Analytics Id

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   copyright: '© All rights reserved.',
   disqusShortname: 'ssangq-1',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-154589980-1',
   useKatex: false,
   menu: [
     {
